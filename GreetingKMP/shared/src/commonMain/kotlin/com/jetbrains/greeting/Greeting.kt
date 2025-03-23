e6037ef965bb561d5d444244d6b4ba82e5038f4e
Denis-Jovith <denisjovitusbuberwa@gmail.com>
2025-03-23 16:15:20 +0300

    deleted the file

--- GreetingKMP/shared/src/commonMain/kotlin/com/jetbrains/greeting/Greeting.kt
+++ GreetingKMP/shared/src/commonMain/kotlin/com/jetbrains/greeting/Greeting.kt
@@ -5,14 +5,9 @@ import kotlin.random.Random
 class Greeting {
      private val platform = getPlatform()
 
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
+    fun greet(): List<String> = buildList{
+
     }
 }
 
 
-fun greet(): String {
-    val firstWord = if (Random.nextBoolean()) "Hi!" else "Hello!"
-
-    return "$firstWord Guess what this is! > ${getPlatform().name.reversed()}"
-}
