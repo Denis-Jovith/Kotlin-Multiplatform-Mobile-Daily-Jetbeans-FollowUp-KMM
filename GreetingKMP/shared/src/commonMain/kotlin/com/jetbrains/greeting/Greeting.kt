1e88cfa399a9a4c6dd6ea82b8cdb3aed3d7d92b9
Denis-Jovith <denisjovitusbuberwa@gmail.com>
2025-03-24 14:15:26 +0300

    modified the file

--- GreetingKMP/shared/src/commonMain/kotlin/com/jetbrains/greeting/Greeting.kt
+++ GreetingKMP/shared/src/commonMain/kotlin/com/jetbrains/greeting/Greeting.kt
@@ -6,7 +6,7 @@ class Greeting {
      private val platform = getPlatform()
 
     fun greet(): List<String> = buildList {
-        add(if (Random.nextBoolean()) "Hi" else "Hello")
+        add(if (Random.nextBoolean()) "Hi!" else "Hello!")
         add("Guess what this is! > ${platform.name.reversed()}!")
     }
 }
